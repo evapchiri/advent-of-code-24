f50918331f664be637c0d75b8380f4e7a9efa30f
evapchiri <138579702+evapchiri@users.noreply.github.com>
2025-11-04 21:49:32 +0000

    Day-2 code tyding w early returns

--- Day-2/day2_code.py
+++ Day-2/day2_code.py
@@ -3,35 +3,20 @@ cleaned_input = [entry.split() for entry in open("./Day-2/input.txt", "r")]
 
 
 def is_safe_report(report):
+## 1 2 7 8 9
+  is_increasing = int(report[0]) < int(report[1])
+  for i in range(len(report) - 1):
+    current_num = int(report[i])
+    next_num = int(report[i + 1])
+    difference = abs(current_num - next_num)
 
-  if int(report[0]) > int(report[1]):
-
-    for i in range(len(report) - 1):
-      current_num = int(report[i])
-      next_num = int(report[i + 1])
-      difference = current_num - next_num
-      if current_num > next_num and abs(difference) in range(4):
+    if is_increasing and current_num < next_num and difference in range(4):
         continue
-      else:
-        return False
-
-    return True
-  
-  elif int(report[0]) < int(report[1]):
-
-    for i in range(len(report) - 1):
-      current_num = int(report[i])
-      next_num = int(report[i + 1])
-      difference = current_num - next_num
-      if current_num < next_num and abs(difference) in range(4):
+    if (not is_increasing) and current_num > next_num and difference in range(4):
         continue
-      else:
-        return False
-      
-    return True
-  
-  else: 
     return False
+  return True
+  
 
 
 count = 0
@@ -47,16 +32,13 @@ def is_safe_with_dampener(report):
   case_checks = []
   for case in possibilities:
     if is_safe_report(case):
-      case_checks.append(True)
-    elif not is_safe_report(case):
-      case_checks.append(False)
+      return True
     
-  # print(case_checks.count(False))
-  # print(case_checks)
-  if True in case_checks:
-    return True
-  else:
-    return False
+  return False
+    
+
+# print(case_checks.count(False))
+# print(case_checks)
 
 # print(cleaned_input[1])
 # print(is_safe_with_dampener(cleaned_input[1]))
